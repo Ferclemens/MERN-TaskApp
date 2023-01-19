9583c8f8bcaa5437ec4318a590150160683057bd
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-01-19 12:20:55 -0300

    add script start in package.json

--- db/database.sql
+++ db/database.sql
@@ -5,6 +5,14 @@ USE companydb;
 CREATE TABLE employee(
 id INT(11) NOT NULL AUTO_INCREMENT,
 name VARCHAR(45) DEFAULT NULL,
+surname VARCHAR(45) DEFAULT NULL,
+position VARCHAR(45) DEFAULT NULL,
 salary INT(5) DEFAULT NULL,
-PRIMARY KEY (id),
+PRIMARY KEY (id)
 );
+
+INSERT INTO employee VALUES
+(1, 'Joe', 'Fernandez','Scrum Master', 3500),
+(2, 'Elva', 'Nanyhn','Product Owner', 2500),
+(3, 'Marcelo', 'Mikisch','Tech Lead', 3500),
+(4, 'Fernando', 'jara','Developer', 4500);
