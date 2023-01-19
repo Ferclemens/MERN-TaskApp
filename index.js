81111fe99c78b2836159d3e5da37e96aa221ea63
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-01-19 10:12:47 -0300

    fix: add surname and position in db & condicional on get employees

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //console.log('hello word');
 import express from 'express'
 import employeesRoutes from './src/routes/employees.routes.js'
+import {PORT} from './src/config.js'
 
 const app = express()
 
 //convertimos a json los datos de las consultas 
 app.use(express.json())
 app.use(employeesRoutes)
-//iniciamos servidor en puerto 3000 (localhost:3000)
-app.listen(3000)
+//respuesta en caso de no encontrar una ruta en el navegador
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'endpoint not found'
+    })
+})
+//iniciamos servidor en puerto 3000 (variable de entorno) - localhost:3000
+app.listen(PORT)
+
+console.log(`app corriendo en puerto ${PORT}`);
+
--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,4 +1,4 @@
-import {pool} from '../../db.js'
+import {pool} from '../db.js'
 
 //siempre la comunicacion con la db es asincrona
 export const getEmployees = async (req, res) => {
@@ -16,8 +16,12 @@ export const getEmployee = async (req, res) => {
     try {
         // guardamos las rows (filas) de la tabla, ya que son los datos que buscamos (si traemos toda la info tambien tenemos los buffers que no nos interesan)
         const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+        //condicional si no se encuentra empleado
+        if(rows[0] === undefined) return res.status(404).json({
+            message:'employee not found'
+        })        
         //buscamos 1 resultado, por eso el indice siempre va a ser 0
-        res.json(rows[0])        
+        res.json(rows[0])
     } catch (error) {
         return res.status(500).json({
             message: 'Something goes wrong'
@@ -26,15 +30,17 @@ export const getEmployee = async (req, res) => {
 }
 
 export const postEmployee = async (req, res) => {
-    const {name, salary} = req.body
+    const {name, surname, position, salary} = req.body
     try {
-      //guardamos en variable el name y salary del request body (destructuración)
-        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?,?)',[name, salary])
+        //guardamos en variable el name y salary del request body (destructuración)
+        const [rows] = await pool.query('INSERT INTO employee (name, surname, position, salary) VALUES (?,?,?,?)',[name, surname, position, salary])
         console.log('post success');
         //console.log(req.body)
         res.send({
             id: rows.insertId,
             name,
+            surname,
+            position,
             salary,
         })  
     } catch (error) {
@@ -46,10 +52,10 @@ export const postEmployee = async (req, res) => {
 
 export const putEmployee = async (req, res) => {
     const {id} = req.params
-    const {name, salary} = req.body
+    const {name, surname, position, salary} = req.body
     try {
         //IFNULL() -> nos permite modificar un solo dato del asiento. ejemplo name o salary. Sin esto debemos modificar todos los datos del asiento o sino la db lo completa con "null".
-        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',[name, salary, id])
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), surname = IFNULL(?, surname), position = IFNULL(?, position), salary = IFNULL(?, salary) WHERE id = ?',[name, surname, position, salary, id])
         //condicional si no se modifica empleado
         if(result.affectedRows === 0 ) return res.status(404).json({
         message: 'employee not found / not modified'
